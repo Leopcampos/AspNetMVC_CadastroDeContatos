31fdf97c99cd056476144e6551f83a735c18b056
Leonardo Campos <leopcampos85@gmail.com>
2023-02-03 19:46:25 -0300

    Implementando o ContatoController, conexão com o BD e Repositorios

---
+++ CadastroDeContatos/Repositório/IContatoRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using CadastroDeContatos.Models;
+using System.Collections.Generic;
+
+namespace CadastroDeContatos.Repositório
+{
+    public interface IContatoRepositorio
+    {
+        List<ContatoModel> BuscarTodos();
+        ContatoModel Adicionar(ContatoModel contato);
+    }
+}
