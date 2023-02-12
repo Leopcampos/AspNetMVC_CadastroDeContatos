2d53cd09e059437a603b9057fed1690133ad4e8a
Leonardo Campos <leopcampos85@gmail.com>
2023-02-11 21:52:49 -0300

    Implementando a restrição para o usuário com perfil padrão não acessar a controller usuario.

--- CadastroDeContatos/Controllers/HomeController.cs
+++ CadastroDeContatos/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CadastroDeContatos.Models;
+﻿using CadastroDeContatos.Filters;
+using CadastroDeContatos.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace CadastroDeContatos.Controllers
 {
+    [PaginaParaUsuarioLogado]
     public class HomeController : Controller
     {
         public IActionResult Index()
--- CadastroDeContatos/Controllers/UsuarioController.cs
+++ CadastroDeContatos/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using CadastroDeContatos.Models;
+﻿using CadastroDeContatos.Filters;
+using CadastroDeContatos.Models;
 using CadastroDeContatos.Repositório;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -6,6 +7,7 @@ using System.Collections.Generic;
 
 namespace CadastroDeContatos.Controllers
 {
+    [PaginaRestritaSomenteAdmin]
     public class UsuarioController : Controller
     {
         private readonly IUsuarioRepositorio _usuarioRepositorio;
