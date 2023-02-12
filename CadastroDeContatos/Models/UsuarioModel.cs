f06efda4a5713e33f2a3059de4afab0a334fe0f9
Leonardo Campos <leopcampos85@gmail.com>
2023-02-12 12:08:36 -0300

    Criptografando a senha do usuário no banco de dados

--- CadastroDeContatos/Models/UsuarioModel.cs
+++ CadastroDeContatos/Models/UsuarioModel.cs
@@ -1,4 +1,5 @@
 ﻿using CadastroDeContatos.Enums;
+using CadastroDeContatos.Helper;
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -7,15 +8,20 @@ namespace CadastroDeContatos.Models
     public class UsuarioModel
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Digite o nome do usuário")]
         public string Nome { get; set; }
+
         [Required(ErrorMessage = "Digite o login do usuário")]
         public string Login { get; set; }
+
         [Required(ErrorMessage = "Digite o e-mail do usuário")]
         [EmailAddress(ErrorMessage = "O e-mail informado não é valido!")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Informe o perfil do usuário")]
         public PerfilEnum? Perfil { get; set; }
+
         [Required(ErrorMessage = "Digite a senha do usuário")]
         public string Senha { get; set; }
         public DateTime DataCadastro { get; set; }
@@ -23,7 +29,12 @@ namespace CadastroDeContatos.Models
 
         public bool SenhaValida(string senha)
         {
-            return Senha == senha;
+            return Senha == senha.GerarHash();
+        }
+
+        public void SetSenhaHash()
+        {
+            Senha = Senha.GerarHash();
         }
     }
 }
